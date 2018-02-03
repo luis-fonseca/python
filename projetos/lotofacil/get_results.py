ffb0c103072db6ba0bdf72181a365aa6fbb1aa68
luís fonseca <fnsc.luis@gmail.com>
2018-02-03 18:50:46 -0200

    correção de bugs

--- projetos/lotofacil/get_results.py
+++ projetos/lotofacil/get_results.py
@@ -1,16 +1,57 @@
+# -*- coding: utf-8 -*
+
 import requests
 import zipfile
-import io
+import htmlmin
+import shutil
+import os
 
-url = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip"
-output = "resultados"
+def minifyHTML(file_to_minify):
 
-response = requests.get(url)
+    output_dir = 'resultados'
+    output_file = os.path.join(output_dir, 'lotofacil.min.html')
 
-if response.status_code == 200:
-    zip_response = zipfile.ZipFile(io.BytesIO(response.content))
-    zip_response.extractall(output)
-else:
-    print("Não foi possível localizar o arquivo de resultados")
+    print 'Minificando arquivo html ...'
+    with open(file_to_minify, 'r') as f:
+        input_html = f.read();
+        input_html = input_html.decode('iso-8859-1')
 
-del response
+        # para não dar problemas com espaços no beautifulsoup
+        output_html = htmlmin.minify(input_html)
+
+        with open(output_file, 'w+') as of:
+            output_html = output_html.encode('utf8')
+            of.write(output_html)
+
+
+def getResults():
+
+    url_results = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip"
+    output_dir = "resultados"
+    output_zip = os.path.join(output_dir, 'resultados.zip')
+    output_html = os.path.join(output_dir, 'resultados.html')
+    html_file = 'D_LOTFAC.HTM'
+
+    response = requests.get(url_results)
+
+    # remove os arquivos do diretorio
+    shutil.rmtree(output_dir)
+    os.mkdir(output_dir)
+
+    if response.status_code == 200:
+        print 'Salvando arquivo zip'
+        with open(output_zip, 'wb') as results:
+            results.write(response.content)
+
+        print 'Extraindo arquivo de resultados'
+        zip = zipfile.ZipFile(output_zip, 'r')
+        zip.extract(html_file, output_dir)
+
+        os.rename(os.path.join(output_dir, html_file), output_html)
+
+        minifyHTML(output_html)
+    else:
+        print("Não foi possível localizar o arquivo de resultados")
+
+    del response
+
